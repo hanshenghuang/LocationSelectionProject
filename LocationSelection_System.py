be9436ea4fe7697c1174f598de2295b135baa538
hanshenghuang <sheng.hshuang@gmail.com>
2025-10-29 01:19:28 +0800

    update system and readme

--- LocationSelection_System.py
+++ LocationSelection_System.py
@@ -1,72 +1,50 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
-
-location_spot=pd.read_csv("location_spot.csv")
-location_MRT=pd.read_csv("location_MRT.csv")
-location_MALL=pd.read_csv("location_MALL.csv")
-location_school=pd.read_csv("location_school.csv")
-population_density_byli=pd.read_csv("population_density.csv",encoding='Big5')
-rent_data=pd.read_csv("台北市店面租賃交易行情107108109.csv",encoding="Big5")
-
-data=pd.read_csv("all_merge_euclidean.csv",encoding='Big5')
-
-
-# In[2]:
-
-
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import confusion_matrix
-from sklearn.utils import shuffle
+import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import math
+from tkinter import Tk, Label, Entry, Button
+
+# import data
+location_spot=pd.read_csv("./raw_data/location_spot.csv")
+location_mrt=pd.read_csv("./raw_data/location_mrt.csv")
+location_mall=pd.read_csv("./raw_data/location_mall.csv")
+location_school=pd.read_csv("./raw_data/location_school.csv")
+population_density_byli=pd.read_csv("./raw_data/population_density.csv",encoding='Big5')
+rent_data=pd.read_csv("./raw_data/rental_data_taipei.csv",encoding="Big5")
+data=pd.read_csv("./processed_data/location_dataset_euclidean.csv",encoding='Big5')
 
 # Build the random forest model
 forest_model=RandomForestClassifier(n_estimators=100,random_state=87,max_depth=4)
-
-X=data[['MRT_count_1000','mall_distance1000','school_distance1000','spot_distance_2000','人口密度','租金']]
-y=data["選址成功"]
-
+X=data[['MRT_count_1000','mall_distance1000','school_distance1000','spot_distance_2000','pop_density','rental_cost']]
+y=data["success"]
 forest_model.fit(X,y)
 
-
-# In[3]:
-
-
-import requests
-import time
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from bs4 import BeautifulSoup
-options = webdriver.ChromeOptions()
-options.add_argument("headless")
-
-# get coordinate and li (village,里) by web crawling
+# get coordinate (latitude and longitude) and li (里 = village) of an address by scraping the Taiwan Map website
 def get_coordinateandli(addr):
-    browser = webdriver.Chrome(executable_path='chromedriver.exe',options=options)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    browser = webdriver.Chrome(options=chrome_options)
     browser.get("http://www.map.com.tw/")
-    search = browser.find_element_by_id("searchWord")
+    search = browser.find_element(By.ID, "searchWord")
     search.clear()
     search.send_keys(addr)
-    browser.find_element_by_xpath("/html/body/form/div[10]/div[2]/img[2]").click() 
+    browser.find_element(By.XPATH, "/html/body/form/table/tbody/tr[1]/td/div[4]/table/tbody/tr/td[2]/table/tbody/tr/td[3]/div").click()
     time.sleep(1)    
-    iframe = browser.find_elements_by_tag_name("iframe")[2]
+    iframe = browser.find_element(By.CLASS_NAME, "winfoIframe")
     browser.switch_to.frame(iframe)
     
-    addr_detail = browser.find_element_by_xpath("/html/body/form/div[4]/table/tbody/tr[1]/td/table/tbody/tr[4]/td").text
+    addr_detail = browser.find_element(By.XPATH, "/html/body/form/div[4]/table/tbody/tr[1]/td/table/tbody/tr[4]/td").text
     index_before = addr_detail.find('區')
     index_after = addr_detail.find('里')
     li = addr_detail[index_before+1:index_after]
-    # print(li)
     
-    coor_btn = browser.find_element_by_xpath("/html/body/form/div[4]/table/tbody/tr[3]/td/table/tbody/tr/td[2]")
+    coor_btn = browser.find_element(By.XPATH, "/html/body/form/div[4]/table/tbody/tr[3]/td/table/tbody/tr/td[2]")
     coor_btn.click()
-    coor = browser.find_element_by_xpath("/html/body/form/div[5]/table/tbody/tr[2]/td")
+    coor = browser.find_element(By.XPATH, "/html/body/form/div[5]/table/tbody/tr[2]/td")
     coor = coor.text.strip().split(" ")
     lat = coor[-1].split("：")[-1]
     log = coor[0].split("：")[-1]
@@ -75,16 +53,11 @@ def get_coordinateandli(addr):
     
     return li, lat, log
 
-
-# In[4]:
-
-
-import math
-
+# calculate the number of MRT stations within 1000 meters
 def count_MRT1000(myX,myY):
     mrt_num=0
-    for i in range(len(location_MRT)):
-        MRT_location=location_MRT.iloc[i,4].split(",")
+    for i in range(len(location_mrt)):
+        MRT_location=location_mrt.iloc[i,4].split(",")
         MRTX=float(MRT_location[1])
         MRTY=float(MRT_location[0])
         d=math.sqrt(math.pow(111.320*math.cos(myY)*(myX-MRTX),2)+math.pow((myY-MRTY)*110.574,2))
@@ -92,10 +65,11 @@ def count_MRT1000(myX,myY):
             mrt_num=mrt_num+1
     return mrt_num
 
+# calculate the number of malls within 1000 meters
 def count_mall1000(myX,myY):
     mall_num=0
-    for i in range(len(location_MALL)):
-        MALL_location=location_MALL.iloc[i,3].split(",")
+    for i in range(len(location_mall)):
+        MALL_location=location_mall.iloc[i,3].split(",")
         MALLX=float(MALL_location[1])
         MALLY=float(MALL_location[0])
         d=math.sqrt(math.pow(111.320*math.cos(myY)*(myX-MALLX),2)+math.pow((myY-MALLY)*110.574,2))
@@ -103,6 +77,7 @@ def count_mall1000(myX,myY):
             mall_num=mall_num+1
     return mall_num
 
+# calculate the number of MRT schools within 1000 meters
 def count_school1000(myX,myY):
     school_num=0
     for i in range(len(location_school)):
@@ -114,6 +89,7 @@ def count_school1000(myX,myY):
             school_num=school_num+1
     return school_num
 
+# calculate the number of famous spots within 2000 meters
 def count_spot2000(myX,myY):
     spot_num=0
     for i in range(len(location_spot)):
@@ -125,30 +101,26 @@ def count_spot2000(myX,myY):
             spot_num=spot_num+1
     return spot_num
 
-
-# In[5]:
-
-
+# get population density
 def get_population_density(li):
     li+="里"
     index=population_density_byli[population_density_byli["里名"]==li].index.values
     index=int(index)
     return population_density_byli.iloc[index][2]
 
-
-# In[6]:
-
-
+# get estimated rent cost
 def get_rent_cost(myX,myY):
     
-    rent_X=rent_data["經度"]
-    rent_Y=rent_data["緯度"]
+    rent_X=rent_data["longitude"]
+    rent_Y=rent_data["latitude"]
     distance=[]
     
+    # calculate the euclidean distance between the input address and each location in the rental data
     for j in range(len(rent_data)):
         d=math.sqrt(math.pow(111.320*math.cos(myY)*(myX-rent_X[j]),2)+math.pow((myY-rent_Y[j])*110.574,2))
         distance.append(d)
-        
+    
+    # take the average rent of the three closest locations as the estimated rent
     first=distance.index(min(distance))
     distance[first]=100
     
@@ -161,13 +133,8 @@ def get_rent_cost(myX,myY):
     cost=(rent_data.iloc[first,5]+rent_data.iloc[second,5]+rent_data.iloc[third,5])/3
     return cost
 
-
-# In[7]:
-
-
-def goanalysis():
-    
-    current_show.config(text="正在分析中...")
+def analyze():
+    current_show.config(text="Analyzing...")
     li_show.config(text="")
     MRT_show.config(text="")
     mall_show.config(text="")
@@ -185,107 +152,108 @@ def goanalysis():
     coordinateY=float(coordinateY)
     coordinateX=float(coordinateX)
     
-    lishow_text="位置："+address+"("+li+")"
+    lishow_text = "Location: " + address + " (" + li + ")"
     li_show.config(text=lishow_text)
     
     MRT_count_1000=count_MRT1000(coordinateX,coordinateY)
-    mrtshow_text="1000m內捷運站數量："+str(MRT_count_1000)
+    mrtshow_text="MRT stations within 1000m: "+str(MRT_count_1000)
     MRT_show.config(text=mrtshow_text)
     
     mall_count_1000=count_mall1000(coordinateX,coordinateY)
-    mallshow_text="1000m內商場數量："+str(mall_count_1000)
+    mallshow_text="Malls within 1000m: "+str(mall_count_1000)
     mall_show.config(text=mallshow_text)
     
     school_count_1000=count_school1000(coordinateX,coordinateY)
-    schoolshow_text="1000m內學校數量："+str(school_count_1000)
+    schoolshow_text="Schools within 1000m: "+str(school_count_1000)
     school_show.config(text=schoolshow_text)
     
     spot_count_2000=count_spot2000(coordinateX,coordinateY)
-    spotshow_text="2000m內景點數量："+str(spot_count_2000)
+    spotshow_text="Attractions within 2000m: "+str(spot_count_2000)
     spot_show.config(text=spotshow_text)
     
     population_density=get_population_density(li)
-    popudenshow_text="人口密度（每平方公里）："+str(round(population_density,2))
+    popudenshow_text="Population density (per km²): "+str(round(population_density,2))
     popden_show.config(text=popudenshow_text)
     
     rent_cost=get_rent_cost(coordinateX,coordinateY)
-    rentcost_text="粗估租金（每平方公尺）："+str(round(rent_cost,2))
+    rentcost_text="Estimated rent (per m²): "+str(round(rent_cost,2))
     renocost_show.config(text=rentcost_text)
     
     X_query=np.array([MRT_count_1000,mall_count_1000,school_count_1000,spot_count_2000,population_density,rent_cost])
     outcome=forest_model.predict_proba(X_query.reshape(1, -1))
-    final_lab_show.config(text="開店成功預測(Predicted Probability)")
+    final_lab_show.config(text="Store Opening Success Prediction=")
     outcome_text=str(round(outcome[0][1]*100,2))+"%"
     final_pred_show.config(text=outcome_text)
     
-    current_show.config(text="分析結果")
+    current_show.config(text="Analysis Result:")
 
-# layout
-from tkinter import*
-import time
+# simple GUI created by TKinter
 win=Tk()
-win.title("Handshake Beverage Stores Location Selection")
+win.title("Location Selection Analysis for Beverage Stores")
 win.geometry("800x600+800+200")
+general_font = "Arial 12"
 
-topic_lab=Label(text="手搖飲料店選址分析")
-topic_lab.config(font="微軟正黑體 24 bold underline")
+topic_lab=Label(text="Location Selection Analysis for Beverage Stores")
+topic_lab.config(font="Arial 20 bold underline")
 topic_lab.place(x=35,y=30)
 
-input_remind=Label(text="請輸入欲開店地址")
-input_remind.config(font="微軟正黑體 12")
+input_remind=Label(text="Please enter the address for the store")
+input_remind.config(font=general_font)
 input_remind.place(x=35,y=100)
 
-input_example=Label(text="（格式如：台北市大安區羅斯福路四段1號）")
-input_example.config(font="微軟正黑體 12")
-input_example.place(x=200,y=100)
+input_example=Label(text="(Example: 台北市大安區羅斯福路四段1號) ")
+input_example.config(font=general_font)
+input_example.place(x=35,y=120)
 
 address_entry=Entry()
-address_entry.config(font="微軟正黑體 15")
-address_entry.place(x=35,y=140,width=500)
+address_entry.config(font=general_font)
+address_entry.place(x=35,y=160,width=500)
 
-btn_analysis=Button(text="ANALYSIS!!",command=goanalysis)
-btn_analysis.config(font="微軟正黑體 12")
+btn_analysis=Button(text="Start Prediction",command=analyze)
+btn_analysis.config(font=general_font)
 btn_analysis.place(x=600,y=110,width=150,height=60)
 
 current_show=Label(text="")
-current_show.config(font="微軟正黑體 12")
+current_show.config(font=general_font)
 current_show.place(x=35,y=200)
 
 li_show=Label(text="")
-li_show.config(font="微軟正黑體 12")
+li_show.config(font=general_font)
 li_show.place(x=35,y=240)
 
 MRT_show=Label(text="")
-MRT_show.config(font="微軟正黑體 12")
+MRT_show.config(font=general_font)
 MRT_show.place(x=35,y=280)
 
 mall_show=Label(text="")
-mall_show.config(font="微軟正黑體 12")
+mall_show.config(font=general_font)
 mall_show.place(x=35,y=320)
 
 school_show=Label(text="")
-school_show.config(font="微軟正黑體 12")
+school_show.config(font=general_font)
 school_show.place(x=35,y=360)
 
 spot_show=Label(text="")
-spot_show.config(font="微軟正黑體 12")
+spot_show.config(font=general_font)
 spot_show.place(x=35,y=400)
 
 popden_show=Label(text="")
-popden_show.config(font="微軟正黑體 12")
+popden_show.config(font=general_font)
 popden_show.place(x=35,y=440)
 
 renocost_show=Label(text="")
-renocost_show.config(font="微軟正黑體 12")
+renocost_show.config(font=general_font)
 renocost_show.place(x=35,y=480)
 
 final_lab_show=Label(text="")
-final_lab_show.config(font="微軟正黑體 12")
+final_lab_show.config(font=general_font)
 final_lab_show.place(x=450,y=240)
 
 final_pred_show=Label(text="")
-final_pred_show.config(font="微軟正黑體 55 underline")
+final_pred_show.config(font="Arial 55 underline")
 final_pred_show.place(x=450,y=280)
 
 win.mainloop()
+
+
 
--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 ## Location Selection Analysis for Beverage Stores -A Case Study of Taipei 
-Bubble tea and handshake beverages stores are a unique feature of Taiwanese culture. Although many new handshake beverages stores open every year, many also close down within just a few years. Beyond brand image and product offerings, we are curious — does the choice of store location also play a significant role in their success? What key factors do relatively successful chain beverage stores consider when selecting their locations?
-
+Bubble tea and handshake beverages stores are a unique feature of Taiwanese culture. Although many new handshake beverages stores open every year, many also close down within just a few years. Beyond brand image and product offerings, we are curious — does the choice of store location also play a significant role in their success? What key factors do relatively successful chain beverage stores consider when selecting their locations?  
 This project aims to analyze the location selection for handshake beverage stores in Taipei using open government data. The models employed include linear regression, logistic regression, decision tree, random forest, and gradient boosting decision tree.
 
 ### Analysis and Report
-Analysis code and description: Please view the [website](https://hanshenghuang.github.io/LocationSelectionProject/LocationSelection_Analysis.html)
+Analysis code and description: Please view the [website](https://hanshenghuang.github.io/LocationSelectionProject/report/LocationSelection_Analysis.html)
 
 ### System Implementation
-This project not only provides a report but also implements a tool that helps beverage companies select optimal locations based on relevant information. By entering a Chinese address in Taipei, users can receive a predicted success probability for that location.
-The image below is the interface of the system. [Here](https://youtu.be/O-T8adiqFRU) is the system demo video.   
-  
+This project not only provides a report but also implements a tool that helps beverage companies select optimal locations based on relevant information. By entering a Chinese address in Taipei, users can receive a predicted success probability for that location.  
+The image below is the interface of the system. [Here](https://youtu.be/wQb_0LrNZpo) is the system demo video.  
 ![image](https://github.com/hanshenghuang/LocationSelectionProject/blob/main/report/interface.png)
 
-### Download the system
-The system is a python application and  was packaged into a single folder, which can be downloaded [here](https://drive.google.com/drive/folders/19RO0BDYo3bNgDFYa31kFZ3YahBSVRWbF). Users just need to download the whole folder and click the execution file "LocationSelection_System.exe". Then, users can input the address and get the predicted outcome.  
-Because the system need to get the latitude and longitude of the address by web crawler, this system need to execute with internet and Chrome (version 99). 
+### Try the system
+This system is a Python application packaged as a standalone .exe file. To try it out, simply download the project and run the executable file LocationSelection_System.exe. After launching the application, you can input an address and receive the predicted outcome.  
+Note: Since the system retrieves the latitude and longitude of the address using a web crawler, an internet connection and Chrome (version 141 or later) are required for proper functionality.
